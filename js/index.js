9d4e26964c33410192374bf7044db876ba9db38c
ana-hamerski <ana.hamerski.vet@gmail.com>
2023-11-13 19:48:36 -0500

    trying to add some more animations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+
+  
+
 // When clicking the button with the arrow icon, the page scrolls to My Projects
 
 $(document).ready(function() {
@@ -57,3 +60,46 @@ $(document).ready(function() {
         reset: true
       });
   });
+
+  // typewriter effect added using a JavaScript library //
+
+  $(document).ready(function() {
+    var lines = [
+        { id: '#line1', text: "hey, there - i'm Ana.", delayAfter: 500 },
+        { id: '#line2', text: "UX & UI Designer", delayAfter: 300 },
+        { id: '#line3', text: "Interdisciplinary Thinker", delayAfter: 300 },
+        { id: '#line4', text: "Researcher", delayAfter: 300 },
+        { id: '#line5', text: "oh...and Animal Whisperer too!", delayAfter: 300 },
+        { id: '#line6', text: "Let's team up and create a digital wonderland of joy and innovation!", delayAfter: 0 }
+      ];
+    
+    var speed = 100; // Speed of the typing in milliseconds
+    var loopDelay = 1000; // Delay before starting the typewriter effect again.
+
+    function typeLine(index) {
+      if (index < lines.length) {
+        var line = lines[index];
+      var i = 0;
+      
+      function typeWriter() {
+        if (i < line.text.length) {
+          $(line.id).html(line.text.substring(0, i + 1));
+          i++;
+          setTimeout(typeWriter, speed);
+        } else {
+          setTimeout(function() { typeLine(index + 1); }, line.delayAfter);
+        }
+      }
+
+      typeWriter();
+    } else {
+        // Once all lines have been typed out, start over after loopDelay
+        setTimeout(function() { 
+          $('.content').find('h1, h2, p').html(''); // Clear the content
+          typeLine(0); // Restart typing
+        }, loopDelay);
+      }
+    }
+
+  typeLine(0);
+});
